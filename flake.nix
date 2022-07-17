1e815600883c99635ccba5df7e05033ebd3e8673
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-07-18 00:45:23 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Home Manager Configuration Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.tars = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+}
