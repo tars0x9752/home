bf56dc496b7b41f3f1a674b3a5d053b2f0894e0e
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-08-01 19:37:59 +0900

    mv init.vim to extraConfig

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,9 +1,72 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.neovim = {
     enable = true;
-  };
 
-  xdg.configFile."nvim/init.vim".source = ./init.vim;
+    plugins = with pkgs.vimPlugins; [
+      vim-airline
+      vim-devicons
+      auto-pairs
+      plenary-nvim
+      telescope-nvim
+      nerdtree
+      iceberg-vim
+      sonokai
+    ];
+
+    extraConfig = ''
+      " --- visual ---
+      colorscheme iceberg
+      set background=dark
+      set termguicolors
+      set title
+      set number
+      set wrap
+      set showmatch
+      set matchtime=3
+      set list "不可視文字の可視化
+      " set listchars=tab:»-,trail:-,extends:»,precedes:«,nbsp:%,eol:↲
+
+      " --- grep ---
+      set ignorecase
+      set smartcase
+      set wrapscan
+      set hlsearch
+      set incsearch
+      set inccommand=split
+
+      " --- indent ---
+      set smartindent
+      set expandtab
+      set tabstop=2
+      set softtabstop=2
+      set shiftwidth=2
+
+      " --- auto complete ---
+      set completeopt=noinsert,menuone,noselect
+      set wildmode=list:longest
+      set infercase
+      set wildmenu
+      au FileType * setlocal formatoptions-=ro
+
+      " --- other ---
+      set mouse=a
+      set clipboard+=unnamedplus
+      set backspace=indent,eol,start
+      set hidden
+      set textwidth=0
+      set encoding=utf-8
+
+      " --- keymap ---
+      " jj as esc
+      inoremap <silent> jj <Esc>
+
+      " Find files using Telescope command-line sugar.
+      nnoremap <leader>ff <cmd>Telescope find_files<cr>
+      nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+      nnoremap <leader>fb <cmd>Telescope buffers<cr>
+      nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+    '';
+  };
 }
