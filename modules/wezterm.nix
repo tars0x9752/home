ce66e2d37d48aa5db9b3392c20687fad43aa4e73
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-08-04 22:58:54 +0900

    wezterm theme

--- modules/wezterm.nix
+++ modules/wezterm.nix
@@ -10,7 +10,7 @@
     
     return {
       font_size = 16,
-      color_scheme = 'Ayu Mirage',
+      color_scheme = 'Chalk', -- 候補: Chester, Chalk, Ayu Mirage など
       window_background_opacity = 0.85,
       hide_tab_bar_if_only_one_tab = true,
     }
