d73fa3bf251d5cf90cd0c88b637f2e20a3b3ad7d
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-08-31 00:10:36 +0900

    cava: gradient

--- modules/cava.nix
+++ modules/cava.nix
@@ -10,13 +10,13 @@
 
     gradient = 1
     gradient_count = 8
-    gradient_color_1 = '#4cc9f0'
-    gradient_color_2 = '#4895ef'
-    gradient_color_3 = '#4361ee'
-    gradient_color_4 = '#3f37c9'
-    gradient_color_5 = '#480ca8'
-    gradient_color_6 = '#7209b7'
-    gradient_color_7 = '#b5179e'
-    gradient_color_8 = '#f72585'
+    gradient_color_1 = '#08D9D6'
+    gradient_color_2 = '#2BC1C6'
+    gradient_color_3 = '#4FA8B5'
+    gradient_color_4 = '#7290A5'
+    gradient_color_5 = '#957794'
+    gradient_color_6 = '#B85F84'
+    gradient_color_7 = '#DC4673'
+    gradient_color_8 = '#FF2E63'
   '';
 }
