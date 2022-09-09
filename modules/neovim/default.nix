e2e4ac2ce666eac70d2007d1278d1a796f11796b
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2022-09-09 18:30:32 +0900

    update nvim completion settings to 0.0.82

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -15,17 +15,15 @@
         nmap <silent> <space>n <Plug>(coc-rename)
         nmap <silent> <space>f <Plug>(coc-format)
 
-        function! CheckBackspace() abort
-          let col = col('.') - 1
-          return !col || getline('.')[col - 1]  =~# '\s'
-        endfunction
+        " use <c-space>for trigger completion
+        inoremap <silent><expr> <c-space> coc#refresh()
 
-        " Use <Tab> and <S-Tab> to navigate the completion list
-        inoremap <silent><expr> <TAB>
-              \ pumvisible() ? "\<C-n>" :
-              \ CheckBackspace() ? "\<TAB>" :
-              \ coc#refresh()
-        inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
+        " use <Tab> and <S-Tab> to navigate the completion list
+        inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+        inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+
+        " use <cr> to confirm completion
+        inoremap <expr> <cr> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
 
         let g:coc_user_config = {
           \"suggest.completionItemKindLabels": {
