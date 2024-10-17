aae5ea1b78e37d71ef83f6081958f45095f9893d
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2024-10-17 18:23:40 +0900

    flake-parts

--- flake.nix
+++ flake.nix
@@ -9,8 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
-
+    flake-parts.url = "github:hercules-ci/flake-parts";
     blesh = {
       url = "https://github.com/akinomyoga/ble.sh/releases/download/v0.4.0-devel3/ble-0.4.0-devel3.tar.xz";
       flake = false;
@@ -18,266 +17,288 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       nixpkgsUnstable,
       home-manager,
-      flake-utils,
+      flake-parts,
       devshell,
       blesh,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+    let
+      hosts = {
+        david-nixos = "david-nixos";
+        lucy-macos = "lucy-macos";
+      };
+    in
+    # https://flake.parts/
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
 
-          overlays = [ devshell.overlays.default ];
-        };
+      imports = [ inputs.devshell.flakeModule ];
 
-        pkgsUnstable = import nixpkgsUnstable {
-          inherit system;
-          config = {
-            allowUnfree = true;
-          };
-        };
-
-        hosts = {
-          david-nixos = "david-nixos";
-          lucy-macos = "lucy-macos";
-        };
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-
-        nixosConfigurations.${hosts.david-nixos} = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [ ./nixos/configuration.nix ];
-        };
-
-        homeConfigurations.${hosts.david-nixos} = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [ ./hosts/david-nixos/home.nix ];
-
-          extraSpecialArgs = {
-            inherit blesh;
-            inherit pkgsUnstable;
-          };
-        };
-
-        homeConfigurations.${hosts.lucy-macos} = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [ ./hosts/lucy-macos/home.nix ];
-
-          extraSpecialArgs = {
-            inherit blesh;
-            inherit pkgsUnstable;
-          };
-        };
-
-        # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-run.html?highlight=apps.%3Csystem%3E#apps
-        # https://nix-community.github.io/home-manager/index.html#sec-flakes-standalone
-        apps = {
-          "activate/${hosts.david-nixos}" =
-            let
-              drv = self.outputs.homeConfigurations.${system}.${hosts.david-nixos}.activationPackage;
-              exePath = "/activate";
-            in
-            {
-              type = "app";
-              program = "${drv}${exePath}";
+      flake =
+        let
+          mkPkgsUnstable =
+            system:
+            import nixpkgsUnstable {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
             };
+        in
+        {
+          nixosConfigurations.${hosts.david-nixos} = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [ ./nixos/configuration.nix ];
+          };
 
-          "activate/${hosts.lucy-macos}" =
-            let
-              drv = self.outputs.homeConfigurations.${system}.${hosts.lucy-macos}.activationPackage;
-              exePath = "/activate";
-            in
-            {
-              type = "app";
-              program = "${drv}${exePath}";
+          homeConfigurations.${hosts.david-nixos} = home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+            modules = [ ./hosts/david-nixos/home.nix ];
+
+            extraSpecialArgs = {
+              inherit blesh;
+              pkgsUnstable = mkPkgsUnstable "x86_64-linux";
             };
+          };
 
-          # just for fun
-          "figlet" = {
-            type = "app";
-            program = "${pkgs.figlet}/bin/figlet";
+          homeConfigurations.${hosts.lucy-macos} = home-manager.lib.homeManagerConfiguration {
+            pkgs = import nixpkgs { system = "aarch64-darwin"; };
+
+            modules = [ ./hosts/lucy-macos/home.nix ];
+
+            extraSpecialArgs = {
+              inherit blesh;
+              pkgsUnstable = mkPkgsUnstable "aarch64-darwin";
+            };
           };
         };
 
-        # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-develop.html
-        # https://github.com/numtide/devshell
-        devShells.default = pkgs.devshell.mkShell {
-          devshell.motd = ''
-            {bold}{14}🔨 My home conigs 🔨{reset}
-            $(type -p menu &>/dev/null && menu)
-          '';
+      perSystem =
+        {
+          pkgs,
+          system,
+          inputs',
+          ...
+        }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
 
-          commands = [
-            # --- Fun ---
-            {
-              name = "dev:hello";
-              category = "Fun";
-              help = "Print a nice hello world";
-              command = ''
-                nix run '.#figlet' -- -f isometric1 -c "Hello World"
-              '';
-            }
+          # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-run.html?highlight=apps.%3Csystem%3E#apps
+          # https://nix-community.github.io/home-manager/index.html#sec-flakes-standalone
+          apps = {
+            "activate/${hosts.david-nixos}" =
+              let
+                drv = self.outputs.homeConfigurations.${hosts.david-nixos}.activationPackage;
+                exePath = "/activate";
+              in
+              {
+                type = "app";
+                program = "${drv}${exePath}";
+              };
 
-            # --- Initial Setup ---
-            {
-              name = "dev:install-david-nixos";
-              category = "Initial Setup";
-              help = "Install home-manager itself and apply the home configuration";
-              command = ''
-                export HOME_MANAGER_BACKUP_EXT=old
-                nix run '.#activate/david-nixos'
-                direnv allow
-              '';
-            }
+            "activate/${hosts.lucy-macos}" =
+              let
+                drv = self.outputs.homeConfigurations.${hosts.lucy-macos}.activationPackage;
+                exePath = "/activate";
+              in
+              {
+                type = "app";
+                program = "${drv}${exePath}";
+              };
 
-            {
-              name = "dev:install-lucy-macos";
-              category = "Initial Setup";
-              help = "Install home-manager itself and apply the home configuration";
-              command = ''
-                export HOME_MANAGER_BACKUP_EXT=old
-                nix run '.#activate/lucy-macos'
-              '';
-            }
+            # just for fun
+            "figlet" = {
+              type = "app";
+              program = "${pkgs.figlet}/bin/figlet";
+            };
+          };
 
-            # --- Home Environment ---
+          # https://flake.parts/options/devshell
+          # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-develop.html
+          # https://github.com/numtide/devshell
+          devshells.default =
+            { config, pkgs, ... }:
             {
-              name = "dev:ls-pkg";
-              category = "Home";
-              help = "List all packages installed in home-manager-path";
-              command = ''
-                home-manager packages
+              devshell.motd = ''
+                {bold}{14}🔨 My home conigs 🔨{reset}
+                $(type -p menu &>/dev/null && menu)
               '';
-            }
-            {
-              name = "dev:ls-gen";
-              category = "Home";
-              help = "List all home environment generations";
-              command = ''
-                home-manager generations
-              '';
-            }
-            {
-              name = "dev:switch";
-              category = "Home";
-              help = "Switch home-manager to apply home config changes";
-              command = ''
-                home-manager switch --flake '.#tars' -b bck --impure
-              '';
-            }
-            {
-              name = "dev:update";
-              category = "Home";
-              help = "Update things";
-              command = ''
-                home-manager switch --flake '.#tars' -b bck --impure --recreate-lock-file
-              '';
-            }
-            {
-              name = "dev:update-nixpkgs";
-              category = "Home";
-              help = "Update nixpkgs only";
-              command = ''
-                nix flake lock --update-input nixpkgs
-                dev:switch
-              '';
-            }
-            {
-              name = "dev:update-lock-only";
-              category = "Home";
-              help = "Update the flake lock file only";
-              command = ''
-                nix flake update
-              '';
-            }
 
-            # --- NixOS ---
-            {
-              name = "dev:os-switch";
-              category = "NixOS";
-              help = "Switch nixos to rebuild and apply `configuration.nix` changes";
-              command = ''
-                sudo nixos-rebuild switch --flake '.#tars' --impure
-              '';
-            }
-            {
-              name = "dev:os-ls-gen";
-              category = "NixOS";
-              help = "List all nixos generations";
-              command = ''
-                sudo nix-env --list-generations --profile /nix/var/nix/profiles/system
-              '';
-            }
+              commands = [
+                # --- Fun ---
+                {
+                  name = "dev:hello";
+                  category = "Fun";
+                  help = "Print a nice hello world";
+                  command = ''
+                    nix run '.#figlet' -- -f isometric1 -c "Hello World"
+                  '';
+                }
 
-            # --- Utility ---
-            {
-              name = "dev:fmt";
-              category = "Utility";
-              help = "Format nix files";
-              command = ''
-                nix fmt
-              '';
-            }
-            {
-              name = "dev:du-svg";
-              category = "Utility";
-              help = "Show what gc-roots are taking space (svg)";
-              command = ''
-                nix-du -s=500MB | tred | dot -Tsvg > store.svg
-              '';
-            }
-            {
-              name = "dev:du-png";
-              category = "Utility";
-              help = "Show what gc-roots are taking space (png)";
-              command = ''
-                nix-du -s=500MB | tred | dot -Tpng > store.png
-              '';
-            }
-            {
-              name = "dev:show-gc-roots";
-              category = "Utility";
-              help = "Show gc-roots";
-              command = ''
-                nix-store --gc --print-roots
-              '';
-            }
-            {
-              name = "dev:gc";
-              category = "Utility";
-              help = "Garbage collection";
-              command = ''
-                sudo nix-collect-garbage
-              '';
-            }
-            {
-              name = "dev:gc-stale";
-              category = "Utility";
-              help = ''Perform garbage collection and delete all generations older than 5 days'';
-              command = ''
-                sudo nix-collect-garbage -d --delete-older-than 5d
-              '';
-            }
-            {
-              name = "dev:gc-all";
-              category = "Utility";
-              help = ''Perform garbage collection and delete all old generations'';
-              command = ''
-                sudo nix-collect-garbage -d
-              '';
-            }
-          ];
+                # --- Initial Setup ---
+                {
+                  name = "dev:install-${hosts.david-nixos}";
+                  category = "Initial Setup";
+                  help = "Install home-manager itself and apply the home configuration";
+                  command = ''
+                    export HOME_MANAGER_BACKUP_EXT=old
+                    nix run '.#activate/${hosts.david-nixos}'
+                    direnv allow
+                  '';
+                }
+                {
+                  name = "dev:install-${hosts.lucy-macos}";
+                  category = "Initial Setup";
+                  help = "Install home-manager itself and apply the home configuration";
+                  command = ''
+                    export HOME_MANAGER_BACKUP_EXT=old
+                    nix run '.#activate/${hosts.lucy-macos}'
+                  '';
+                }
+
+                # --- home-manager switch ---
+                {
+                  name = "dev:switch-${hosts.david-nixos}";
+                  category = "Home";
+                  help = "Switch home-manager to apply home config changes";
+                  command = ''
+                    home-manager switch --flake '.#${hosts.david-nixos}' -b bck --impure
+                  '';
+                }
+                {
+                  name = "dev:switch-${hosts.lucy-macos}";
+                  category = "Home";
+                  help = "Switch home-manager to apply home config changes";
+                  command = ''
+                    home-manager switch --flake '.#${hosts.lucy-macos}' -b bck --impure
+                  '';
+                }
+
+                # --- home-manager ls ---
+                {
+                  name = "dev:ls-pkg";
+                  category = "Home";
+                  help = "List all packages installed in home-manager-path";
+                  command = ''
+                    home-manager packages
+                  '';
+                }
+                {
+                  name = "dev:ls-gen";
+                  category = "Home";
+                  help = "List all home environment generations";
+                  command = ''
+                    home-manager generations
+                  '';
+                }
+
+                # --- update ---
+                {
+                  name = "dev:update";
+                  category = "Home";
+                  help = "Update the flake lock file only";
+                  command = ''
+                    nix flake update
+                    echo "plz switch after this"
+                  '';
+                }
+                {
+                  name = "dev:update-nixpkgs";
+                  category = "Home";
+                  help = "Update nixpkgs only";
+                  command = ''
+                    nix flake lock --update-input nixpkgs
+                    echo "plz switch after this"
+                  '';
+                }
+
+                # --- NixOS ---
+                {
+                  name = "dev:os-switch";
+                  category = "NixOS";
+                  help = "Switch nixos to rebuild and apply `configuration.nix` changes";
+                  command = ''
+                    sudo nixos-rebuild switch --flake '.#${hosts.david-nixos}' --impure
+                  '';
+                }
+                {
+                  name = "dev:os-ls-gen";
+                  category = "NixOS";
+                  help = "List all nixos generations";
+                  command = ''
+                    sudo nix-env --list-generations --profile /nix/var/nix/profiles/system
+                  '';
+                }
+
+                # --- Utility ---
+                {
+                  name = "dev:fmt";
+                  category = "Utility";
+                  help = "Format nix files";
+                  command = ''
+                    nix fmt
+                  '';
+                }
+                {
+                  name = "dev:du-svg";
+                  category = "Utility";
+                  help = "Show what gc-roots are taking space (svg)";
+                  command = ''
+                    nix-du -s=500MB | tred | dot -Tsvg > store.svg
+                  '';
+                }
+                {
+                  name = "dev:du-png";
+                  category = "Utility";
+                  help = "Show what gc-roots are taking space (png)";
+                  command = ''
+                    nix-du -s=500MB | tred | dot -Tpng > store.png
+                  '';
+                }
+                {
+                  name = "dev:show-gc-roots";
+                  category = "Utility";
+                  help = "Show gc-roots";
+                  command = ''
+                    nix-store --gc --print-roots
+                  '';
+                }
+                {
+                  name = "dev:gc";
+                  category = "Utility";
+                  help = "Garbage collection";
+                  command = ''
+                    sudo nix-collect-garbage
+                  '';
+                }
+                {
+                  name = "dev:gc-stale";
+                  category = "Utility";
+                  help = ''Perform garbage collection and delete all generations older than 5 days'';
+                  command = ''
+                    sudo nix-collect-garbage -d --delete-older-than 5d
+                  '';
+                }
+                {
+                  name = "dev:gc-all";
+                  category = "Utility";
+                  help = ''Perform garbage collection and delete all old generations'';
+                  command = ''
+                    sudo nix-collect-garbage -d
+                  '';
+                }
+              ];
+            };
         };
-      }
-    );
+
+    };
 }
